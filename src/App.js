83910977550c55d7376ccecaf55f6b465ef89c65
divya1viswanath <divyarugmini798@gmail.com>
2022-05-19 09:09:23 +0530

    header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import  'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Employee from './component/Employee';
+import { BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Employee/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'exact element={<Employee/>}/>
+        </Routes>                                                                                   
+      </BrowserRouter>
       
     </div>
   );
---
+++ src/component/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>Header
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <Link class="navbar-brand" to={'/'}>Social Media </Link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <Link class="nav-link active" to='/'>Employee</Link>
+                </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
