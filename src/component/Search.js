e987c0455fb5cf4fcaa998a11fbe9b4c7cc7615c
divya1viswanath <divyarugmini798@gmail.com>
2022-05-19 10:07:21 +0530

    value

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Search = () => {
+    var [empid,setEmpid]=useState("")
+    
+const subData =() => {
+    const data={"empid":empid}
+    console.log(data)
+}
   return (
     <div>
         <Header/>
@@ -11,12 +17,11 @@ const Search = () => {
                     <div className="row g-3">
                     
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <label for="" className="form-label">EMP ID</label>
-                            <input type="text" className="form-control"/>
-                            <input type="date" name="" id="" class="form-control"/>
+                            <label for="" className="form-label">EMP ID</label>                            
+                            <input onChange={(e)=>{setEmpid(e.target.value)}} type="text" name="" id="" class="form-control"/>
                         </div>
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                            <button className="btn btn-primary">Search</button>
+                            <button onClick={subData} value="search" className="btn btn-success">Search</button>
                         </div>
                     </div>
                 </div>
